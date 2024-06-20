8064ba801086ebfc5b3c5603a74f92ae17feaa8b
박하민 <parkhamint@naver.com>
2024-06-20 19:37:25 +0900

    Initial Commit

---
+++ src/test/java/com/project/member_management/MemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.member_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
